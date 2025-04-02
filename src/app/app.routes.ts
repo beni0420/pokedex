52d36abaa8c44eafcbb9125732d44018625d6499
maria.benito <maria.benito@timestampgroup.com>
2025-04-02 17:41:41 +0200

    pegar ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { TablaPokemonComponent } from './components/tabla-pokemon/tabla-pokemon.component';
 
-export const routes: Routes = [{}];
+export const routes: Routes = [{path: '', component: TablaPokemonComponent}];
