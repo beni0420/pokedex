418ff8c1b77a6162ce7657ed42102904c18b8752
maria.benito <maria.benito@timestampgroup.com>
2025-04-02 17:25:18 +0200

    proyecto base

---
+++ src/app/components/tabla-pokemon/tabla-pokemon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-pokemon',
+  imports: [],
+  templateUrl: './tabla-pokemon.component.html',
+  styleUrl: './tabla-pokemon.component.scss'
+})
+export class TablaPokemonComponent {
+
+}
